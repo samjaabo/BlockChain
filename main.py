919d83e38c6ea0780e41d150cd0cca540719d298
Said Amjaabou <samjaabo@e3r3p13.1337.ma>
2024-09-13 17:00:17 +0100

    disclaimer

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
+"""
+Disclaimer:
+
+Please note that I am only learning about Web3
+and blockchain technology, and my involvement is purely for educational purposes.
+I do not engage in any illegal activities.
+and I fully comply with the laws and regulations of Morocco.
+"""
+
 
 import hashlib
 import time
--- test.py
+++ test.py
@@ -1,3 +1,13 @@
+"""
+Disclaimer:
+
+Please note that I am only learning about Web3
+and blockchain technology, and my involvement is purely for educational purposes.
+I do not engage in any illegal activities.
+and I fully comply with the laws and regulations of Morocco.
+"""
+
+
 import base58
 import ecdsa
 from ecdsa import SigningKey, SECP256k1
